1bffb4e72dc1474ca4f3269d61ceb897632de1bb
Aman Waghmare <amanwaghmare311@gmail.com>
2024-06-03 12:25:19 +0530

    routes are up to date

--- src/controlers/user.controler.js
+++ src/controlers/user.controler.js
@@ -4,6 +4,8 @@ import { User } from "../model/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import multer from "multer";
+import mongoose from "mongoose";
 
 const genAccessAndRefreshToken = async (userid) => {
   try {
@@ -250,76 +252,210 @@ const changePassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "password changed successfully"));
 });
 
-
 const getCurrentUser = asyncHandler(async (req, res) => {
- return res
- .status(200)
- .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
 
-})
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
 
-const updateAccountDetails = asyncHandler( async(req,res)=>{
-  const {fullName,email} = req.body;
+  if (!(fullName || email)) throw new ApiError(401, "all fields are necessory");
 
-  if(!(fullName || email)) throw new ApiError(401,"all fields are necessory")
-
-  const user = User.findByIdAndUpdate(req.user._id,{
-    $set:{
-      fullName,
-      email
-    }
-  },{validateBeforeSave:false}).select("-password")
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    { validateBeforeSave: false }
+  ).select("-password");
 
   return res
-  .status(200)
-  .json(
-    new ApiResponse(200,user,"User details are updated successfuly")
-  )
+    .status(200)
+    .json(new ApiResponse(200, user, "User details are updated successfuly"));
+});
 
-})
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar file is missing");
 
-const updateUserAvatar = asyncHandler(async (req,res) =>{
- const avatarLocalPath = req.file?.path
- if(!avatarLocalPath) throw new ApiError(400,"Avatar file is missing");
- 
- const avatar = await uploadOnCloudinary(avatarLocalPath)
- if(!avatar.url) throw new ApiError(400,"error while uploading avatar");
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar.url) throw new ApiError(400, "error while uploading avatar");
 
- const user = await User.findByIdAndUpdate(req.user?._id,{
-  $set:{
-    avatar:avatar.url
-  },
- 
- } ,{new:true}).select("-password")
- 
- return res
- .status(200)
- .json(
-   new ApiResponse(200,user,"Avatar image updated successfuly")
- )
-})
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
 
-const updateUserCoverImage = asyncHandler(async (req,res) =>{
- const coverImageLocalPath = req.file?.path
- if(!coverImageLocalPath) throw new ApiError(400,"CoverImage file is missing");
- 
- const coverImage = await uploadOnCloudinary(coverImageLocalPath)
- if(!coverImage.url) throw new ApiError(400,"error while uploading avatar");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar image updated successfuly"));
+});
 
- const user = await User.findByIdAndUpdate(req.user?._id,{
-  $set:{
-    coverImage:coverImage.url
-  },
- 
- } ,{new:true}).select("-password")
- 
-return res
-.status(200)
-.json(
-  new ApiResponse(200,user,"Cover image updated successfuly")
-)
-})
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+  console.log(coverImageLocalPath);
 
+  if (!coverImageLocalPath)
+    throw new ApiError(400, "CoverImage file is missing");
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!coverImage.url) throw new ApiError(400, "error while uploading avatar");
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover image updated successfuly"));
+});
+
+const getUserCannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) throw new ApiError(401, "username is required");
+
+  //aggregation pipeline to join and get processed output from multiple collections in mongodb
+  const channel = await User.aggregate([
+    //to use aggregate pipeline returns array
+    {
+      $match: {
+        //use to find a document in collection
+        username: username?.toLowerCase(), //by username
+      },
+    }, //this filter returns single document
+    {
+      $lookup: {
+        // to join other collection
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel", // this will look for channel field in all documents where _id is matched
+        as: "subscribers",
+      }, //calculate all subscribers for current user - list of subscribers
+    },
+    {
+      $lookup: {
+        // to join other collection
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber", // this will look for subsriber field in all documents where _id is matched
+        as: "subscribedTo",
+      }, //calculate all subscribedTo channels for current user - list of channels
+    },
+    {
+      // we have data in above fields but they are seperate to add them
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers", //calculate size
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            //to check some condition
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] }, //$in to check(iterate) contains in array or object, and check if _id is present unnder subscribers collection and subscriber field
+            then: true, //if true then return true
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        //to return only required fields to manage network bandwidth
+        fullName: 1,
+        username: 1,
+        email: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) throw new ApiError(404, "channel does not exist");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "User Channel Fetched Sussessfully "));
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        //as mongoose did not return mongodb id in _id it returns only string, but while sending db query it internaly converts it unlike in aggreate pipeline
+        _id: new mongoose.Types.ObjectId(req.user._id), //this convers a _id string to mongoose obj that helps to fetch db while using aggregation
+      }, //got user
+    },
+    {
+      $lookup: {
+        //to find all the videos having _id
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          // additions sub pipe lines
+          {
+            $lookup: {
+              //to find the owener of the video by looking into user collections _id field
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner", // to make obj from the owner field that is a array, make convenient to frontend
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetch succesfully"
+      )
+    );
+});
 
 export {
   home,
@@ -330,5 +466,8 @@ export {
   changePassword,
   getCurrentUser,
   updateAccountDetails,
-  updateUserCoverImage
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserCannelProfile,
+  getWatchHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { home, registerUser, logInUser,logOutUser,refreshAccessToken } from "../controlers/user.controler.js";
+import {
+  home,
+  registerUser,
+  logInUser,
+  logOutUser,
+  refreshAccessToken,
+  updateUserAvatar,
+  updateUserCoverImage,
+  changePassword,
+  getCurrentUser,
+  updateAccountDetails,
+  getUserCannelProfile,
+  getWatchHistory,
+} from "../controlers/user.controler.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import {verifyJwt} from "../middlewares/auth.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/home").get(home);
@@ -26,5 +39,23 @@ router.route("/login").post(logInUser);
 router.route("/logout").post(verifyJwt, logOutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJwt, changePassword);
+
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/coverImage")
+  .patch(verifyJwt, upload.single(), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJwt, getUserCannelProfile);
+
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
