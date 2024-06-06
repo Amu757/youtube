ba8758961948ea7f62ae231ce29ec12b3615c2df
Aman Waghmare <amanwaghmare311@gmail.com>
2024-06-06 15:25:29 +0530

    updated some models

---
+++ src/model/like.model.js
@@ -0,0 +1,23 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const likeSchema = new Schema({
+     video:{
+        type:Schema.Types.ObjectId,
+        ref:"Video"
+    },
+    comment:{
+        type:Schema.Types.ObjectId,
+        ref:"Comment"
+    },
+    tweet:{
+        type:Schema.Types.ObjectId,
+        ref:"Tweet"
+    },
+    LikedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+export const Like = mongoose.model("Like",likeSchema)
