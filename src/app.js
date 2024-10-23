fb39b228da2bd2a3ff6fc70796cb5b935689c107
Aman  Waghmare <93338002+Amu757@users.noreply.github.com>
2024-10-23 10:30:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,12 +4,17 @@ import cookieparser from 'cookie-parser'  //to access and set cookies on the ser
 
 const app =  express()
 
-app.use(cors({
+const corsOptions = {
     origin:process.env.CORS_ORIGIN,   // allow req from the origin mentioned
-    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
-}))
+}
+
+// Enable CORS for all requests
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))  // to parse url data, extended means can be nested object
