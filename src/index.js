2c1445fe6872824e2078337081c29968366730a9
Aman Waghmare <amanwaghmare311@gmail.com>
2024-10-24 10:41:14 +0530

    changed app.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ connectDB() // its a async function that returns promise
     // app.listen(process.env.PORT || 8000,()=>{  // avialable port on server or hosting system
     // console.log("server is running on localhost:" + process.env.PORT);
     console.log("mongodb connection is done running on server")
-    }) 
+    // }) 
 })
   .catch((error) => {
     console.log("mongodb connection failed !!", error);
